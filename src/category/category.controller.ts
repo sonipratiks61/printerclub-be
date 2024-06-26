2319b4b7bbb37da7d1be96f82bacca8990af753a
vidhushibackbencher <vidhushi.jain@backbenchertechnologies.com>
2024-06-26 17:02:52 +0530

    resolve the bugs

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -33,7 +33,7 @@ export class CategoryController {
     @Res() res,
   ) {
     try {
-      const userId = req.user.id; // Access req.user.id from the request object
+      const userId = req.user.id; 
       const { message } = await this.categoryService.create(createCategoryDto, userId);
       this.responseService.sendSuccess(res, message);
     } catch (error) {
