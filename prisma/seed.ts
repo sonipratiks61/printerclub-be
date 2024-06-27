4eefeb6b1798e0bf7b418f641e8f565591124d36
vidhushibackbencher <vidhushi.jain@backbenchertechnologies.com>
2024-06-27 13:44:03 +0530

    Added the seeder of all models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-
+import * as bcrypt from 'bcrypt';
 // initialize Prisma Client
 const prisma = new PrismaClient();
 const attributes = [
@@ -112,8 +112,177 @@ async function main() {
       updatedAt: new Date('2023-07-09'),
     },
   });
+  const userData = {
+    businessName: "John",
+    name: "John",
+    email: "user@gmail.com",
+    mobileNumber: "1111111111",
+    password: "password123",  // This should be replaced with the actual password input
+    isActive: false,
+    gstNumber: "1234567890",
+    acceptTerms: true,
+    roleId: 1,
+    createdAt: new Date('2023-07-09'),
+    updatedAt: new Date('2023-07-09'),
+  };
+
+  // Hash the password
+  const hashedPassword1 = await bcrypt.hash(userData.password, 8);
+  userData.password = hashedPassword1;
+
+  
+  const data = {
+    businessName: "Admin",
+    name: "Admin",
+    email: "Admin@gmail.com",
+    mobileNumber: "1212121212",
+    password: "password123", 
+    isActive: true,
+    gstNumber: "1234567890",
+    acceptTerms: true,
+    roleId: 2,
+    createdAt: new Date('2023-07-09'),
+    updatedAt: new Date('2023-07-09'),
+  };
+
+  // Hash the password
+  const hashedPassword2= await bcrypt.hash(data.password, 8);
+  data.password = hashedPassword2;
+
+await prisma.user.upsert({
+    where: { email: userData.email },
+    update: {
+      ...userData,
+      updatedAt: new Date(),  
+    },
+    create: userData,
+  });
+   await prisma.user.upsert({
+    where: { email: data.email },
+    update: {
+      ...data,
+      updatedAt: new Date(),
+    },
+    create: data,
+  });
+ 
+  await prisma.category.upsert({
+    where: {
+      id: 1
+    },
+    create: {
+      name: "Category1",
+      type: "service",
+      description: "This is Category 1",
+      userId: 1, // Replace with actual user ID
+      createdAt: new Date('2023-07-09'),
+      updatedAt: new Date('2023-07-09'),
+    },
+    update: {}
+  })
+  await prisma.category.upsert({
+    where: {
+      id: 2
+    },
+    create: {
+      name: "SubCategory",
+      type: "service",
+      description: "This is subCategory 1",
+      parentId:1,
+      userId: 1, // Replace with actual user ID
+      createdAt: new Date('2023-07-09'),
+      updatedAt: new Date('2023-07-09'),
+    },
+    update: {}
+  })
+  await prisma.product.upsert({
+    where: {
+      id: 1
+    },
+    create: {
+      name: "Product 1",
+      isMeasurementRequired:true,
+      isFitmentRequired:true,
+      description: "This is product 1",
+      categoryId:2,
+      userId: 1,
+      quantity:{
+        type:"text",
+        min:234,
+        max:323
+      },
+      price:"3452",
+      createdAt: new Date('2023-07-09'),
+      updatedAt: new Date('2023-07-09'),
+    },
+    update: {}
+  })
+  await prisma.product.upsert({
+    where: {
+      id: 2
+    },
+    create: {
+      name: "Product 2",
+      isMeasurementRequired:true,
+      isFitmentRequired:true,
+      description: "This is product 2",
+      categoryId:2,
+      userId: 1,
+      quantity:{
+        type:"dropDown",
+       options:[23,43,54]
+      },
+      price:"3452",
+      createdAt: new Date('2023-07-09'),
+      updatedAt: new Date('2023-07-09'),
+    },
+    update: {}
+  })
+  await prisma.productAttribute.upsert({
+    where: {
+      id: 1
+    },
+    create: {
+      name: "ProductAttribute 1",
+      productId:1,
+      type:"dropDown",
+       options:["23","43","54"],
+    },
+    update: {}
+  })
+  await prisma.productAttribute.upsert({
+    where: {
+      id: 2
+    },
+    create: {
+      name: "ProductAttribute 2",
+      productId:1,
+      type:"text",
+      
+    },
+    update: {}
+  })
+  await prisma.address.upsert({
+    where: {
+      id: 1
+    },
+    create: {
+      address: "Shyam Hill",
+      city:"Bhopal",
+      pinCode:"462001",
+      userId:1,
+      state:"Madhya Pradesh",
+      country:"India",
+      createdAt: new Date('2023-07-09'),
+      updatedAt: new Date('2023-07-09'),
+
+      
+    },
+    update: {}
+  })
   console.log('insert Successfully');
 }
+
 main()
   .catch((e) => {
     console.error(e);
