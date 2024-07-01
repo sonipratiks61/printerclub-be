60a9e7e55fcc7ee4607a96bc018de3373979af4c
vidhushibackbencher <vidhushi.jain@backbenchertechnologies.com>
2024-07-01 16:01:25 +0530

    added the capability crud

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -44,6 +44,7 @@ async function main() {
     update: {}, // No need to update anything if it already exists
     create: {
       name: 'User',
+
       createdAt: new Date('2023-07-09'),
       updatedAt: new Date('2023-07-09'),
     },
@@ -54,6 +55,7 @@ async function main() {
     update: {}, // No need to update anything if it already exists
     create: {
       name: 'Admin',
+
       createdAt: new Date('2023-07-09'),
       updatedAt: new Date('2023-07-09'),
     },
