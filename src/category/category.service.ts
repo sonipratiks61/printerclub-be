1665197a2f973c78cd3f923ab905412c33df81d4
vidhushibackbencher <vidhushi.jain@backbenchertechnologies.com>
2024-07-02 16:44:39 +0530

    resolve category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -54,46 +54,46 @@ export class CategoryService {
     });
   }
 
-  async findAll(includeSubCategory: boolean) {
-    if (includeSubCategory) {
-      const data = await this.prisma.category.findMany({
-        where: {
-          parentId: null
-        },
 
-        include: {
-          subCategories: {
-            select: {
-              id: true,
-              name: true,
-              parentId: true,
-              type: true,
-              description: true
+    async findAll(includeSubCategory: boolean) {
+      if (includeSubCategory) {
+        const data = await this.prisma.category.findMany({
+          where: {
+           parentId: null },
+      
+          include: {
+            subCategories: {
+              select: {
+                id: true,
+                name: true,
+                parentId: true,
+                type: true,
+                description: true
+              }
             }
           }
-        }
-      })
-
-      const formatted = data.flatMap(category => [
-        {
-          id: category.id,
-          name: category.name,
-          parentId: category.parentId,
-          type: category.type,
-          description: category.description
-        },
-        ...category.subCategories.map(subCategory => ({
-          id: subCategory.id,
-          name: category.name,
-          subCategory: subCategory.name,
-          parentId: subCategory.parentId,
-          type: subCategory.type,
-          description: subCategory.description
-        }))
-      ])
-
-      return formatted
-    }
+        })
+      
+        const formatted = data.flatMap(category => [
+          {
+            id: category.id,
+            name: category.name,
+            parentId: category.parentId,
+            type: category.type,
+            description: category.description
+          },
+          ...category.subCategories.map(subCategory => ({
+            id: subCategory.id,
+            name: category.name,
+            subCategory: subCategory.name,
+            parentId: subCategory.parentId,
+            type: subCategory.type,
+            description: subCategory.description
+          }))
+        ])
+      
+        return formatted 
+      }
     else {
       return this.prisma.category.findMany(
         {
@@ -110,8 +110,7 @@ export class CategoryService {
         });
     }
   }
-
-
+            
   async update(id: number, updateCategoryDto: UpdateCategoryDto) {
     if (updateCategoryDto.parentId) {
       const parentCategory = await this.prisma.category.findUnique({
