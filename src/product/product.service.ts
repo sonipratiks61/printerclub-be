b77697c469f70881c0a88f7ec2b00fa6158428e4
vidhushibackbencher <vidhushi.jain@backbenchertechnologies.com>
2024-07-11 13:11:35 +0530

    refactore the code

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -83,6 +83,7 @@ export class ProductService {
       if (product.category.parentId) {
         parentCategory = await this.categoryService.findOne(product.category.parentId);
       }
+  
       return {
         id: product.id,
         categoryId: product.category.id,
