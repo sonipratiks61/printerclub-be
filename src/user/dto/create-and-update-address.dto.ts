209da4452c14ba1d14c37076b50c5fdf4bd3bf58
vidhushibackbencher <vidhushi.jain@backbenchertechnologies.com>
2024-07-22 13:49:05 +0530

    Edited the pincode message

--- src/user/dto/create-and-update-address.dto.ts
+++ src/user/dto/create-and-update-address.dto.ts
@@ -6,6 +6,51 @@ import {
   IsOptional,
 } from 'class-validator';
 
+export class CreateAddressDto {
+  @ApiProperty({
+    description: 'The country where the user resides',
+    example: 'United States',
+  })
+  @IsNotEmpty({ message: 'Country cannot be empty' })
+  @IsString({ message: 'Country must be a string' })
+  country: string;
+  @ApiProperty({
+    description: 'The state or province where the user resides',
+    example: 'California',
+  })
+  @IsNotEmpty({ message: 'State cannot be empty' })
+  @IsString({ message: 'State must be a string' })
+  state: string;
+
+  @ApiProperty({
+    description: 'The city where the user resides',
+    example: 'San Francisco',
+  })
+  @IsNotEmpty({ message: 'City cannot be empty' })
+  @IsString({ message: 'City must be a string' })
+  city: string;
+
+  @ApiProperty({
+    description: "The postal code of the user's address",
+    example: '941038',
+  })
+  @IsNotEmpty({ message: 'PinCode cannot be empty.' })
+  @IsPostalCode('IN',{
+  message:'Invalid Pincode'
+  })
+  pinCode: string;
+
+  @ApiProperty({
+    description:
+      'The full address line including street name, building number, etc.',
+    example: '123 Main St, Apt 101',
+  })
+  @IsOptional()
+  @IsNotEmpty({ message: 'Address cannot be empty.' })
+  @IsString({ message: 'Address must be a string' })
+  address: string;
+}
+
 export class UpdateAddressDto {
   @ApiProperty({
     description: 'The country where the user resides',
@@ -39,7 +84,7 @@ export class UpdateAddressDto {
   })
   @IsOptional()
   @IsNotEmpty({ message: 'PinCode cannot be empty.' })
-  @IsPostalCode('IN')
+  @IsPostalCode('IN',{message:"Invalid Pincode"})
   pinCode: string;
 
   @ApiProperty({
