ccaa0a21575a01ca12c7f4ffa12665cb1a94d6cf
prachi21backbencher <prachi.sahu@backbenchertechnologies.com>
2024-07-25 16:30:19 +0530

    route

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,6 +2,7 @@ import {
   IsBoolean,
   IsDefined,
   IsEmail,
+  IsInt,
   IsNotEmpty,
   IsOptional,
   IsString,
@@ -9,10 +10,11 @@ import {
   ValidateNested,
 } from 'class-validator';
 
-import { CreateAddressDto } from './create-and-update-address.dto';
+import { CreateAddressDto, UpdateAddressDto } from './create-and-update-address.dto';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsTenDigitNumber } from 'utils/validation/phoneNumberValidation';
+
 
 export class CreateUserDto {
   @ApiProperty({
@@ -75,6 +77,11 @@ export class CreateUserDto {
   @IsBoolean({ message: 'Accept Terms must be Boolean ' })
   acceptTerms: boolean;
 
+  @IsOptional()
+  @IsNotEmpty({ message: 'RoleId cannot be empty.' })
+  @IsInt({ message: 'RoleId must be a Integer' })
+  roleId: number;
+
   @ApiProperty({
     type: [CreateAddressDto],
     description: 'List of addresses associated with the user',
@@ -94,4 +101,94 @@ export class CreateUserDto {
   @IsDefined()
   @Type(() => CreateAddressDto)
   addresses: CreateAddressDto[];
+}
+
+export class UpdateUserDto {
+  @ApiProperty({
+    description: 'The business name of the user',
+    example: 'Acme Corp',
+  })
+  @IsOptional()
+  @IsNotEmpty({ message: 'Business Name cannot be empty.' })
+  @IsString({ message: 'Business Name must be a string' })
+  businessName: string;
+
+  @ApiProperty({
+    description: 'The full name of the user',
+    example: 'John Doe',
+  })
+  @IsOptional()
+  @IsNotEmpty({ message: 'Name cannot be empty.' })
+  @IsString({ message: 'Name must be a string' })
+  name: string;
+
+
+  @ApiProperty({
+    description: 'The RoleId of the user',
+    example: 1,
+  })
+  @IsOptional()
+  @IsNotEmpty({ message: 'RoleId cannot be empty.' })
+  @IsInt({ message: 'RoleId must be a Integer' })
+  roleId: number;
+
+  @ApiProperty({
+    description: 'The mobile phone number of the user',
+    example: '123-456-7890',
+  })
+  @IsNotEmpty({ message: 'Mobile number must not be empty' })
+  @IsString({ message: 'Mobile number must be a string' })
+  @IsTenDigitNumber({ message: 'Mobile number must be a 10-digit number' })
+  @IsOptional()
+  mobileNumber: string;
+
+  @ApiProperty({
+    description: 'The password for the user account',
+    example: 'strongPassword123!',
+    minLength: 8,
+  })
+  @IsOptional()
+  @MinLength(8)
+  @IsNotEmpty({ message: 'Password cannot be empty.' })
+  password: string;
+
+  @ApiProperty({
+    description: 'The GST number of the user (optional)',
+    example: '27AAAAA0000A1Z5',
+    required: false,
+  })
+  @IsOptional()
+  @IsString({ message: 'GST Number must be a string' })
+  gstNumber?: string;
+
+  @ApiProperty({
+    description: 'Whether the user accepts terms and conditions',
+    example: true,
+  })
+  @IsOptional()
+  @IsNotEmpty({ message: 'Accept Terms must not be empty' })
+  @IsBoolean({ message: 'Accept Terms must be Boolean ' })
+  acceptTerms: boolean;
+
+  @ApiProperty({
+    type: [CreateAddressDto],
+    description: 'List of addresses associated with the user',
+    isArray: true,
+    example: [
+      {
+        country: 'India',
+        state: 'Maharashtra',
+        city: 'Mumbai',
+        pinCode: '400001',
+        address: '101, High street, Near Central Park',
+      },
+    ],
+  })
+  
+  @IsOptional()
+  @IsNotEmpty({ message: 'Addresses must not be empty.' })
+  @ValidateNested({ each: true })
+  @IsDefined()
+  @Type(() => UpdateAddressDto)
+  addresses: UpdateAddressDto;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { CreateUserDto, UpdateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { RoleService } from 'src/role/role.service';
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService,
+    private roleService: RoleService) { }
 
   async findAllUsersWithAddresses() {
     const users = await this.prisma.user.findMany({
@@ -58,5 +61,153 @@ export class UserService {
       where: { id: userId },
       data: { isActive },
     });
+  }
+
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async userCreateByAdmin(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 8);
+    const userEmail = await this.prisma.user.findFirst({
+      where: {
+        email: createUserDto.email
+
+      }
+    });
+    const userMobileNumber = await this.prisma.user.findFirst({
+      where: {
+        mobileNumber: createUserDto.mobileNumber
+
+      }
+    })
+    const role = await this.roleService.findOne(createUserDto.roleId)
+    if (!role) {
+      throw new NotFoundException('Role not found')
+    }
+    const errors: { email?: string; mobileNumber?: string } = {};
+
+    if (userEmail) {
+      errors.email = "Email is Already Exist";
+    }
+    if (userMobileNumber) {
+      errors.mobileNumber = "MobileNumber is Already Exist";
+    }
+    if(Object.keys(errors).length>0) {
+      throw new ConflictException(errors);
+    }
+    
+    const data = await this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        businessName: createUserDto.businessName,
+        roleId: createUserDto.roleId,
+        gstNumber: createUserDto.gstNumber,
+        email: createUserDto.email,
+        mobileNumber: createUserDto.mobileNumber,
+        acceptTerms: createUserDto.acceptTerms,
+        isActive: false,
+        password: hashedPassword,
+        addresses: {
+          create: createUserDto.addresses
+        }
+      },
+      select: {
+        id: true,
+        name: true,
+        businessName: true,
+        email: true,
+        mobileNumber: true,
+        gstNumber: true,
+        roleId: true,
+        isActive: true,
+        acceptTerms: true,
+        addresses: {
+          select: {
+            address: true,
+            city: true,
+            state: true,
+            country: true,
+            pinCode: true
+          }
+        },
+        role: {
+          select: {
+            id: true,
+            name: true,
+          }
+        }
+      }
+    })
+    return data;
+  }
+
+  async updateUserByAdmin(id: number, updateUserDto: UpdateUserDto) {
+    const role = await this.roleService.findOne(updateUserDto.roleId);
+    if (!role) {
+      throw new NotFoundException("Role not Found")
+    }
+    const address = await this.prisma.address.findFirst({
+      where: {
+        userId: id,
+      },
+      select: {
+        id: true,
+
+      }
+    });
+    const data = await this.prisma.user.update({
+      where: {
+        id: id
+      },
+      data: {
+        name: updateUserDto.name,
+        businessName: updateUserDto.businessName,
+        mobileNumber: updateUserDto.mobileNumber,
+        gstNumber: updateUserDto.gstNumber,
+        roleId: updateUserDto.roleId,
+        addresses: {
+          update: {
+            where: {
+              id: address.id
+            },
+            data: {
+              address: updateUserDto.addresses.address,
+              city: updateUserDto.addresses.city,
+              state: updateUserDto.addresses.state,
+              pinCode: updateUserDto.addresses.pinCode,
+              country: updateUserDto.addresses.country,
+            }
+          }
+        },
+
+      },
+      select: {
+        id: true, name: true, businessName: true,
+
+        addresses: {
+          select: {
+            id: true,
+            address: true,
+            city: true,
+            state: true,
+            pinCode: true,
+            country: true,
+          }
+        }, role: {
+          select: {
+            id: true,
+            name: true,
+
+          }
+        }
+      }
+    })
+    return data;
+
   }
 }
